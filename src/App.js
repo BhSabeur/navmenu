6f346c2f7ed987b0b919ed482db7d3251151b974
BhSabeur <bhsabeur@gmail.com>
2020-02-11 23:45:17 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
+  const linksArray = [
+    { title: "Home", subMenu: null },
+    {
+      title: "Services",
+      subMenu: ["For Entrepreneurs", "For Students", "For Hobbyists"]
+    },
+    { title: "Contact", subMenu: null }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar linksArray={linksArray} />
     </div>
   );
 }
